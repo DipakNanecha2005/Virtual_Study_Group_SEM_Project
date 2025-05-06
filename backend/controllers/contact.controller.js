10c098963d26c847f4802dbb0977b8261c1e49c6
EarlyBird <dnanecha8@gmail.com>
2025-05-06 12:29:17 +0530

    added route to  get all contacts

--- backend/controllers/contact.controller.js
+++ backend/controllers/contact.controller.js
@@ -31,3 +31,22 @@ export const searchContacts = async (req, res) => {
         });
     }
 }
+
+export const getAllContacts = async (req, res) => {
+    try {
+        const users = await UserModel.find(
+            { _id: { $ne: req.userId } },
+            "firstName lastNamme _id email"
+        );
+
+        const contacts = users.map((user) => ({
+            label: (user.firstName && user.lastName) ? `${user.firstName} ${user.lastName}` : user.email,
+            value: user._id
+        }));
+
+        return res.status(200).json({ contacts });
+    } catch (error) {
+        console.log({ error });
+        return res.status(500).send("Internal Server error");
+    }
+}
--- backend/routes/contact.route.js
+++ backend/routes/contact.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { authUser } from "../middlewares/auth.middleware.js";
-import { searchContacts } from "../controllers/contact.controller.js";
+import { getAllContacts, searchContacts } from "../controllers/contact.controller.js";
 
 const router = express.Router();
 
 router.post("/search", authUser, searchContacts);
+router.get("/get-all-contacts", authUser, getAllContacts);
 
 export default router;
